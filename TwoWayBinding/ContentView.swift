a2560b3591f3ba2f8d8f3452552a4ecbaef234cc
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-08 23:34:51 +0100

    implemented project version

--- TwoWayBinding/ContentView.swift
+++ TwoWayBinding/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  @State private var chargePercent = 0.0
+  @State private var scaleFactor = 0.0
+  @State private var isCharged = false
+  
+  var body: some View {
+    GeometryReader { geo in
+      VStack {
+        Spacer()
+        RoundedRectangle(cornerRadius: 60)
+          .fill(.appBlue)
+          .frame(width: geo.size.width, height: geo.size.height * scaleFactor)
+          .overlay(RoundedRectangle(cornerRadius: 60)
+            .stroke(.appBrown, lineWidth: 2)
+            .overlay(Text(isCharged ? "Fully Charged!" : "")
+              .font(.title)
+              .fontWeight(.semibold)
+              .foregroundStyle(.white)
+            )
+          )
+          .animation(.easeInOut, value: scaleFactor)
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("Charged to %\(chargePercent.formatted())")
+            .font(.headline)
+            .fontWeight(.semibold)
+            .foregroundStyle(.appBrown)
+          Button("Charge!") {
+            if chargePercent >= 99 {
+              chargePercent = 100
+              isCharged = true
+            } else {
+              chargePercent += 5.88
+            }
+            if scaleFactor < 0.85 {
+              scaleFactor += 0.05
+            }
+            
+          }
+          .font(.headline)
+          .fontWeight(.semibold)
+          .buttonStyle(.borderedProminent)
+          .tint(.appBlue)
+          .overlay(Capsule().stroke(.appBrown, lineWidth: 2))
         }
         .padding()
+      }
+      
     }
+    .padding()
+  }
 }
 
 #Preview {
