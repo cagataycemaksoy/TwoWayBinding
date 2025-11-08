9d48a9d07272fbb0cb190cb851026b8248f5d3d7
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-08 22:44:08 +0100

    Initial Commit

---
+++ TwoWayBinding/TwoWayBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingApp.swift
+//  TwoWayBinding
+//
+//  Created by Cem Aksoy on 8.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
